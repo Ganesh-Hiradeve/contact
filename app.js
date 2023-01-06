0c5dc99025c2a2bfeaa12c31433d6fc75bc3e89e
Ganesh-Hiradeve <2020bcs092@sggs.ac.in>
2023-01-06 23:38:19 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+
+const express = require("express");
+const path = require("path");
+const app = express();
+var mongoose = require('mongoose');
+const bodyparser = require("body-parser");
+const { dirname } = require("path");
+mongoose.connect('mongodb+srv://Ganesh_Hiradeve:ganesh%40123@cluster0.jj8t8qw.mongodb.net/datasave?retryWrites=true&w=majority', { useNewUrlParser: true });
+const port = 2000;
+
+var noteSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    subject: String,
+    message: String
+});
+
+var note = mongoose.model('data', noteSchema);
+
+
+
+// app.use('/assets', express.static('assets')); 
+app.use('/style.css',express.static(__dirname + '/style.css')); 
+
+
+app.use(express.urlencoded())
+
+
+app.set('view engine', 'html'); 
+app.set(__dirname + '/index.html'); 
+
+
+app.get('/', (req, res) => { 
+    const params = {}
+    res.sendFile(__dirname + '/index.html');
+})
+app.post('/data', (req, res) => {
+    var myData = new note(req.body);
+    myData.save().then(() => {
+        res.send("Submitted Successfully")
+    }).catch(() => {
+        res.status(400).send("This item has not been send to the database")
+    });
+   
+})
+
+
+app.listen(port, () => {
+    console.log(`The application started successfully on port ${port}`)
+})
+
+
+
+
+
+
